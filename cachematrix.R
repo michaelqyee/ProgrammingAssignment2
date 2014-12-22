44706cf556f6a02fef31c733a741d31154f763cd
michaelqyee <michaelyee@MichaelYeesAir.home>
2014-12-21 21:21:19 -0500

    Updates of cachematrix stub code
    Filled out and commented code for makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix must be initialized with a type matrix object in order to use cacheSolve. it is assumed that the passed matrix is invertable. the matrix saved to makeCacheMatrix can be inverted by passing the object to cacheSolve
+
 ## Write a short comment describing this function
 
+## makeCacheMatrix creates an object with the capabilities to (1) set the value of the matrix, (2) get the value of the matrix, (3) set the value of the matrix inverse, and (4) get the value fo the matrix inverse
+
 makeCacheMatrix <- function(x = matrix()) {
+	
+	inverse_matrix <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse_matrix <<- NULL
+	}
+	get <- function() x
+	set_inverse <- function(inverse) inverse_matrix <<- inverse
+	get_inverse <- function() inverse_matrix
+	list (set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
+	
 
 }
 
 
 ## Write a short comment describing this function
 
+## cacheSolve takes a matrix object that has been initialized by the makeCacheMatrix function, returns the cached inverse if previously calculated, and otherwise calculates and returns the inverse
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inverse_matrix <- x$get_inverse()
+        if (!is.null(inverse_matrix)) {
+        	message("getting cached data")
+        	return(inverse_matrix)
+        }
+        
+        data <- x$get()
+        inverse_matrix <- solve(data)
+        x$set_inverse(inverse_matrix)
+        inverse_matrix
+        
 }
